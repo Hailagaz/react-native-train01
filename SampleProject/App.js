ea8d8060d506df84778476b174d6b793deb0c9a4
Oleh Cherniavskyi <94393608+Hailagaz@users.noreply.github.com>
2023-08-15 10:42:15 +0300

    add Drawer navigation

--- SampleProject/App.js
+++ SampleProject/App.js
@@ -1,51 +1,36 @@
-import * as React from 'react';
 import { View, StyleSheet } from 'react-native';
-
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import WelcomeScreen from './components/WelcomeScreen';
 import LoginScreen from './components/LoginScreen';
 import { NavigationContainer } from '@react-navigation/native';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-	return (
-		<>
-			<NavigationContainer>
-				<View style={styles.container}>
-					<LittleLemonHeader />
-					<Tab.Navigator
-						screenOptions={({ route }) => ({
-							tabBarIcon: ({ size }) => {
-								let iconName;
-								if (route.name === 'Welcome') {
-									iconName = 'ios-home';
-								} else if (route.name === 'Login') {
-									iconName = 'ios-enter';
-								}
-								return <Ionicons name={iconName} size={size} />;
-							},
-						})}
-						initialRouteName="Login">
-						<Tab.Screen name="Welcome" component={WelcomeScreen} />
-						<Tab.Screen name="Login" component={LoginScreen} />
-					</Tab.Navigator>
-				</View>
-				<View style={styles.footerContainer}>
-					<LittleLemonFooter />
-				</View>
-			</NavigationContainer>
-		</>
-	);
+  return (
+    <>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <LittleLemonHeader />
+          <Drawer.Navigator useLegacyImplementation initialRouteName="Login">
+            <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+            <Drawer.Screen name="Login" component={LoginScreen} />
+          </Drawer.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <LittleLemonFooter />
+        </View>
+      </NavigationContainer>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#333333',
-	},
-	footerContainer: { backgroundColor: '#333333' },
+  container: {
+    flex: 1,
+    backgroundColor: '#333333',
+  },
+  footerContainer: { backgroundColor: '#333333' },
 });
